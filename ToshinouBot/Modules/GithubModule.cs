aa530bb89854e53eff9c48c35327313992fa9137
Pendramon <pendramon2@gmail.com>
2018-05-28 00:15:24 +0200

    Added: Branch parameter to Github command

--- ToshinouBot/Modules/GithubModule.cs
+++ ToshinouBot/Modules/GithubModule.cs
@@ -7,16 +7,23 @@ namespace ToshinouBot.Modules
     public class GithubModule : ModuleBase<SocketCommandContext>
     {
         [Command("github")]
-        public async Task Github()
+        public async Task Github(string branch = "")
         {
             var embed = new Discord.EmbedBuilder();
+            string Branch = branch.ToLower();
             embed.WithTitle("Github");
-            embed.AddInlineField("Master (Recommended)", "https://github.com/Gagong/Toshinou-Revamped/tree/master");
-            embed.AddInlineField("Beta", "https://github.com/Gagong/Toshinou-Revamped/tree/beta");
+
+            if (Branch == "master") {
+                embed.AddInlineField("Master", "https://github.com/Gagong/Toshinou-Revamped/tree/master");
+            } else if (Branch == "beta") {
+                embed.AddInlineField("Beta", "https://github.com/Gagong/Toshinou-Revamped/tree/beta");
+            } else {
+                embed.AddInlineField("Master (Recommended)", "https://github.com/Gagong/Toshinou-Revamped/tree/master");
+                embed.AddInlineField("Beta", "https://github.com/Gagong/Toshinou-Revamped/tree/beta");
+            }
             embed.Color = Color.Green;
 
             await ReplyAsync("", false, embed);
         }
-        
     }
 }

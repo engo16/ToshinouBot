a48c7e5b78adb3c214b3095f28c4e55ac1a14042
Pendramon <pendramon2@gmail.com>
2018-05-27 21:52:02 +0200

    Added: Github command.

---
+++ ToshinouBot/Modules/GithubModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+
+namespace ToshinouBot.Modules
+{
+    public class GithubModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("github")]
+        public async Task Github()
+        {
+            var embed = new Discord.EmbedBuilder();
+            embed.WithTitle("Github");
+            embed.AddInlineField("Master (Recommended)", "https://github.com/Gagong/Toshinou-Revamped/tree/master");
+            embed.AddInlineField("Beta", "https://github.com/Gagong/Toshinou-Revamped/tree/beta");
+            embed.Color = Color.Green;
+
+            await ReplyAsync("", false, embed);
+        }
+        
+    }
+}
